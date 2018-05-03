7f5a1e305966c7830618acc9b46d7d68b498d747
MAYKOL-SL <maykoljdslsl@gmail.com>
2018-05-02 20:00:40 -0400

    modificado principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './comunicationcdh/login/login.component';
+import { PrincipalComponent } from './comunicationcdh/principal/principal.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+     path: 'principal',
+     component: PrincipalComponent
+   },
+   {
+     path: '**', pathMatch: 'full', redirectTo: 'login'
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
---
+++ src/app/comunicationcdh/comunicationcdh.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LoginComponent, PrincipalComponent]
+})
+export class ComunicationcdhModule { }
