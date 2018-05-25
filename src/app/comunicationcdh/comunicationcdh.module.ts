d4be74898e7ce3eeacb56e78eb9805f91cea1004
noelito010 <noelito010@gmail.com>
2018-05-25 07:47:04 -0400

    chat en proceso

--- src/app/comunicationcdh/comunicationcdh.module.ts
+++ src/app/comunicationcdh/comunicationcdh.module.ts
@@ -9,8 +9,12 @@ import { PerfilComponent } from './perfil/perfil.component';
 import { PublicacionesComponent } from './publicaciones/publicaciones.component';
 import { PublicacionImgUnComponent } from './publicacion-img-un/publicacion-img-un.component';
 import { PublicacionImgMuchComponent } from './publicacion-img-much/publicacion-img-much.component';
+import { ChatGeneralComponent } from './chat/chat-general.component'
 import { ChatComponent } from './list-chat/chat.component';
 import { NotifiComponent } from './notificaciones/notificaciones.component';
+import { TareasComponent } from './tareas/tareas.component';
+import { MensajeComponent } from './mensaje/mensaje.component';
+import { ContactoComponent } from './contactos/contacto.component';
 
 
 @NgModule({
@@ -20,6 +24,7 @@ import { NotifiComponent } from './notificaciones/notificaciones.component';
   declarations: [LoginComponent, PrincipalComponent, CalendarComponent, 
                   MenuComponent, HeaderComponent, PerfilComponent, PublicacionesComponent,
                   PublicacionImgUnComponent, PublicacionImgMuchComponent, NotifiComponent,
-                  ChatComponent]
+                  TareasComponent, ChatComponent, ChatGeneralComponent, MensajeComponent,
+                ContactoComponent]
 })
 export class ComunicationcdhModule { }
--- src/app/comunicationcdh/mensaje/mensaje.ts
+++ src/app/comunicationcdh/mensaje/mensaje.ts
@@ -1,10 +1,9 @@
-export class Contact{
+export class Mensaje{
     constructor(
         public id: number,
         public nombre: string,
         public imagen: string,
         public estado: boolean,
-        public aviso: boolean,
         public mensaje: string,
         public ultConx: string,
         public numMensaje: number
